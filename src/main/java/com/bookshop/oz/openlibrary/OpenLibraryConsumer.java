cc8bb8dc86f345796c577db6bf5b095bc7f47b7c
KorolER <elizavetakorol2005@gmail.com>
2024-09-30 16:30:20 +0300

    делаем букшоп

---
+++ src/main/java/com/bookshop/oz/openlibrary/OpenLibraryConsumer.java
@@ -0,0 +1,56 @@
+package com.bookshop.oz.openlibrary;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.bookshop.oz.openlibrary.model.Document;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+@Component
+public class OpenLibraryConsumer {
+	private final String basicURL = "https://openlibrary.org/search.json?";
+	private final String searchedByTitle = "q=%s";
+	private final String fields = "fields=cover_i,key,title,author_name,editions,editions.publisher,editions.language,editions.isbn";
+	private final String limit = "limit=%d";
+	
+	private final Integer PAGE_LIMIT = 10;
+	
+	public List<Document> getBooks(String line) throws IOException, InterruptedException, URISyntaxException
+	{
+		line = new String(line.getBytes(), StandardCharsets.UTF_8);
+		String URL = String.format(basicURL+searchedByTitle+"&"+fields+"&"+limit, line, PAGE_LIMIT);
+        
+		HttpRequest getRequest = HttpRequest.
+			newBuilder().
+			uri(new URI(URL)).GET().header("Accept", "application/json").build();
+		HttpResponse<String> response = HttpClient.newHttpClient().send(getRequest, BodyHandlers.ofString());
+		
+		JsonElement jsonElement = JsonParser.parseString(response.body());
+        JsonArray docsArray = jsonElement.getAsJsonObject().getAsJsonArray("docs");
+
+        Gson gson = new GsonBuilder().create();
+        List<Document> documents = new ArrayList<>();
+
+        for (JsonElement docElement : docsArray) {
+            Document document = gson.fromJson(docElement, Document.class);
+            documents.add(document);
+        }
+        
+        documents.stream().forEach(System.out::println);
+        return documents;
+	}
+}
