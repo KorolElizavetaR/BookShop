433b21e7fd00685b3924c52a2ff16746a5cc1abd
KorolER <elizavetakorol2005@gmail.com>
2024-11-13 10:16:54 +0300

    Finished with db dump

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -9,7 +9,7 @@ CREATE TABLE location_points
   is_storage bool NOT NULL DEFAULT false
 );
 
-INSERT INTO location_points VALUES ('Минск', 'ул. проспект Независимости, 25', true);
+INSERT INTO location_points VALUES ('00000','Минск', 'ул. проспект Независимости, 25', true);
 INSERT INTO location_points(location_id, city, address) VALUES 
 	('10000','Минск', 'Игуменский тракт, 14'), 
 	('10001', 'Минск', 'пр. Независимости, 92'),
@@ -40,38 +40,34 @@ CREATE TABLE user_authorities
 	PRIMARY KEY (person_id, user_authority)
 );
 
-BEGIN;
-
 WITH inserted_persons AS (
-    INSERT INTO person (first_name, last_name, email, bpassword, current_location)
+    INSERT INTO person (first_name, last_name, email, phone, bpassword, current_location)
     VALUES 
-    ('Андрей', 'Васильев', 'andrey.vasilyev@example.com', '$2a$10$D0X9rLjrxepZg6oNUvAlpehHUJMd67Zux1.lMznFwCBxbvxYORmSa', 10000), -- Оригинальный пароль: Andrey2024!
-    ('Светлана', 'Михайлова', 'svetlana.mikhaylova@example.com', '$2a$10$nN8RlHb45OGvGMSixzN9lu2AEMViFZwHHu1M7y1zFE.dGwn2mpya6', 10004), -- Оригинальный пароль: Svetlana1234@
-    ('Артем', 'Николаев', 'artem.nikolaev@example.com', '$2a$10$f1Ok8IYX4L6JpjY4v78UgeXoYr1X5AvW8hNllJ67/fBk4EOgU4fDK', 10004), -- Оригинальный пароль: Artem4567#
-    ('Оксана', 'Попова', 'oksana.popova@example.com', '$2a$10$H13oIvJ1mGRd1Z2HQnpFS.tanod6CT.r5GJcyQgl9hNiw09DqKzVS', 10004), -- Оригинальный пароль: Oksana7890!
-    ('Георгий', 'Соколов', 'georgiy.sokolov@example.com', '$2a$10$c3SvhgZWkT/rTeoL7w/Aue8RtIoM2pjsXMOwKtt/Pm9Hi6q4exJhC', 50000), -- Оригинальный пароль: Georgiy2024@
-    ('Екатерина', 'Лебедева', 'ekaterina.lebedeva@example.com', '$2a$10$GvL7ISzy1.pchDck6FAIpeTRu0t/JPrspROEkH/av9M3HVUu7T2va', 10004), -- Оригинальный пароль: Ekaterina1234!
-    ('Максим', 'Романов', 'maksim.romanov@example.com', '$2a$10$Z6y2RoHApGjqOiTEV5IpXeoT8NyZAWPUivxW5DWgNEMVfFXYwaZqK', 10004), -- Оригинальный пароль: Maksim5678!
-    ('Полина', 'Павлова', 'polina.pavlova@example.com', '$2a$10$pW92WV5NsMHiYm0KuFy.luZXHZAsxs19LNdvUBGjC8GzAijIxEWHu', 10000), -- Оригинальный пароль: Polina2024!
-    ('Никита', 'Киселев', 'nikita.kiselev@example.com', '$2a$10$4BPlu1IvMgM1k6TQ4AXXUOifLmpKsR2Cj2f3uhbvE9F3U7.qrTx0G', 10004), -- Оригинальный пароль: Nikita1234@
-    ('Татьяна', 'Григорьева', 'tatyana.grigoryeva@example.com', '$2a$10$efpr4SOJoF1.K9yxb6oEQuWgLkck/J6IbuMsoxx6k5ijQoMIxAbP6', 10004),
-    ('Иван', 'Иванов', 'ivan.ivanov@example.com', '+375(29)123-45-67', '$2a$10$k1XzKdCZz.E9RYhvThTyBuROIfACGvCJgKH.eZNeCnG5YmOpIo5.y', 10001), -- Оригинальный пароль: Ivan2024!
-    ('Алексей', 'Смирнов', 'alexey.smirnov@example.com', '+375(33)234-56-78', '$2a$10$Q9wFFltzSdhjqfT/QgD2NOHO0gQ5NXuy5BeGZ2y.XVJNLNnHlVYui', 10002), -- Оригинальный пароль: Alex1234@
-    ('Мария', 'Кузнецова', 'maria.kuznetsova@example.com', '+375(25)345-67-89', '$2a$10$4Pn3PMexl7xM5LPj9jrnVOj/yjbqOgb/xJdrFif9KeN0np1ALziK.', 10002), -- Оригинальный пароль: Maria4567#
-    ('Елена', 'Новикова', 'elena.novikova@example.com', '+375(44)456-78-90', '$2a$10$tM8EkJu7nq/RylZ5fhOQuuDqljEBYhzPKwCHnMCdY.m5VHDIorEmG', 20000), -- Оригинальный пароль: Elena7890!
-    ('Дмитрий', 'Ковалев', 'dmitry.kovalev@example.com', '+375(29)567-89-01', '$2a$10$wTj.ZnwrjPZIEGlq58jkL.I9Bd0ddZGPEkRvuhgT5CkWrgkmL9hPS', 10004), -- Оригинальный пароль: Dima2024@
-    ('Ольга', 'Сидорова', 'olga.sidorova@example.com', '+375(33)678-90-12', '$2a$10$SGePEFA5uN1mSb8mFBuwle.RTu7yF8MZ8uPU5fuDdCJY1Do5E1v8G', 10005), -- Оригинальный пароль: Olga1234!
-    ('Сергей', 'Морозов', 'sergey.morozov@example.com', '+375(25)789-01-23', '$2a$10$YY/T6.aUtTQC9vBxvjNUO.Iv/xTnvk8tB9J5RqT0Zz2lCrEQGuPCq', 10004), -- Оригинальный пароль: Sergey5678!
-    ('Анна', 'Петрова', 'anna.petrova@example.com', '+375(44)890-12-34', '$2a$10$X94ERt9I/V4Bgh0RIlsziOvbHtP0MeJ/EULssjtsCr5aFJzWbUv8a', 50001), -- Оригинальный пароль: Anna2024!
-    ('Виктор', 'Федоров', 'victor.fedorov@example.com', '+375(29)901-23-45', '$2a$10$0EOA5v1sGHpbhjyxPmTh.eJ0X7mWfdDreCZ9f4X6EQuw0oTflklra', 30000), -- Оригинальный пароль: Victor1234@
-    ('Юлия', 'Зайцева', 'yulia.zaitseva@example.com', '+375(33)012-34-56', '$2a$10$G.Xb/PwXAVTD5SCeh8DfKu3n2mHf.HZ9FUBq3PRa/GO.y1ryXjT/q', 50001)-- Оригинальный пароль: Tatyana2024!
+    ('Андрей', 'Васильев', 'andrey.vasilyev@example.com', '+375(44)123-45-67', '$2a$10$D0X9rLjrxepZg6oNUvAlpehHUJMd67Zux1.lMznFwCBxbvxYORmSa', 10000), -- Original password: Andrey2024!
+    ('Светлана', 'Михайлова', 'svetlana.mikhaylova@example.com', '+375(29)234-56-78', '$2a$10$nN8RlHb45OGvGMSixzN9lu2AEMViFZwHHu1M7y1zFE.dGwn2mpya6', 10004), -- Original password: Svetlana1234@
+    ('Артем', 'Николаев', 'artem.nikolaev@example.com', '+375(29)345-67-89', '$2a$10$f1Ok8IYX4L6JpjY4v78UgeXoYr1X5AvW8hNllJ67/fBk4EOgU4fDK', 10004), -- Original password: Artem4567#
+    ('Оксана', 'Попова', 'oksana.popova@example.com', '+375(29)456-78-90', '$2a$10$H13oIvJ1mGRd1Z2HQnpFS.tanod6CT.r5GJcyQgl9hNiw09DqKzVS', 10004), -- Original password: Oksana7890!
+    ('Георгий', 'Соколов', 'georgiy.sokolov@example.com', '+375(44)567-89-01', '$2a$10$c3SvhgZWkT/rTeoL7w/Aue8RtIoM2pjsXMOwKtt/Pm9Hi6q4exJhC', 50000), -- Original password: Georgiy2024@
+    ('Екатерина', 'Лебедева', 'ekaterina.lebedeva@example.com', '+375(29)678-90-12', '$2a$10$GvL7ISzy1.pchDck6FAIpeTRu0t/JPrspROEkH/av9M3HVUu7T2va', 10004), -- Original password: Ekaterina1234!
+    ('Максим', 'Романов', 'maksim.romanov@example.com', '+375(29)789-01-23', '$2a$10$Z6y2RoHApGjqOiTEV5IpXeoT8NyZAWPUivxW5DWgNEMVfFXYwaZqK', 10004), -- Original password: Maksim5678!
+    ('Полина', 'Павлова', 'polina.pavlova@example.com', '+375(29)890-12-34', '$2a$10$pW92WV5NsMHiYm0KuFy.luZXHZAsxs19LNdvUBGjC8GzAijIxEWHu', 10000), -- Original password: Polina2024!
+    ('Никита', 'Киселев', 'nikita.kiselev@example.com', '+375(44)901-23-45', '$2a$10$4BPlu1IvMgM1k6TQ4AXXUOifLmpKsR2Cj2f3uhbvE9F3U7.qrTx0G', 10004), -- Original password: Nikita1234@
+    ('Татьяна', 'Григорьева', 'tatyana.grigoryeva@example.com', '+375(29)012-34-56', '$2a$10$efpr4SOJoF1.K9yxb6oEQuWgLkck/J6IbuMsoxx6k5ijQoMIxAbP6', 10004), -- Original password: Tatyana2024!
+    ('Иван', 'Иванов', 'ivan.ivanov@example.com', '+375(29)123-45-67', '$2a$10$k1XzKdCZz.E9RYhvThTyBuROIfACGvCJgKH.eZNeCnG5YmOpIo5.y', 10001), -- Original password: Ivan2024!
+    ('Алексей', 'Смирнов', 'alexey.smirnov@example.com', '+375(33)234-56-78', '$2a$10$Q9wFFltzSdhjqfT/QgD2NOHO0gQ5NXuy5BeGZ2y.XVJNLNnHlVYui', 10002), -- Original password: Alex1234@
+    ('Мария', 'Кузнецова', 'maria.kuznetsova@example.com', '+375(25)345-67-89', '$2a$10$4Pn3PMexl7xM5LPj9jrnVOj/yjbqOgb/xJdrFif9KeN0np1ALziK.', 10002), -- Original password: Maria4567#
+    ('Елена', 'Новикова', 'elena.novikova@example.com', '+375(44)456-78-90', '$2a$10$tM8EkJu7nq/RylZ5fhOQuuDqljEBYhzPKwCHnMCdY.m5VHDIorEmG', 20000), -- Original password: Elena7890!
+    ('Дмитрий', 'Ковалев', 'dmitry.kovalev@example.com', '+375(29)567-89-01', '$2a$10$wTj.ZnwrjPZIEGlq58jkL.I9Bd0ddZGPEkRvuhgT5CkWrgkmL9hPS', 10004), -- Original password: Dima2024@
+    ('Ольга', 'Сидорова', 'olga.sidorova@example.com', '+375(33)678-90-12', '$2a$10$SGePEFA5uN1mSb8mFBuwle.RTu7yF8MZ8PU5fuDdCJY1Do5E1v8G', 10004), -- Original password: Olga1234!
+    ('Сергей', 'Морозов', 'sergey.morozov@example.com', '+375(25)789-01-23', '$2a$10$YY/T6.aUtTQC9vBxvjNUO.Iv/xTnvk8tB9J5RqT0Zz2lCrEQGuPCq', 10004), -- Original password: Sergey5678!
+    ('Анна', 'Петрова', 'anna.petrova@example.com', '+375(44)890-12-34', '$2a$10$X94ERt9I/V4Bgh0RIlsziOvbHtP0MeJ/EULssjtsCr5aFJzWbUv8a', 50001), -- Original password: Anna2024!
+    ('Виктор', 'Федоров', 'victor.fedorov@example.com', '+375(29)901-23-45', '$2a$10$0EOA5v1sGHpbhjyxPmTh.eJ0X7mWfdDreCZ9f4X6EQuw0oTflklra', 30000), -- Original password: Victor1234@
+    ('Юлия', 'Зайцева', 'yulia.zaitseva@example.com', '+375(33)012-34-56', '$2a$10$G.Xb/PwXAVTD5SCeh8DfKu3n2mHf.HZ9FUBq3PRa/GO.y1ryXjT/q', 50001) -- Original password: Tatyana2024!
     RETURNING person_id
 )
 
 INSERT INTO user_authorities (person_id, user_authority)
 SELECT person_id, 'ROLE_CUSTOMER' FROM inserted_persons;
-
-COMMIT;
 
 INSERT INTO person (first_name, last_name, email, bpassword, current_location) VALUES 
     ('Антонина', 'Антюшеня', 'tonyaantush2000@gmail.com', '$2a$12$2FE57DtSZaQvMUAr3LBWCe6lPqO/CZtKxOrx1A5KDzHQFGqYMfd.6', 10004), -- antushenya@ shop_assistant customer
@@ -106,7 +102,7 @@ INSERT INTO book VALUES
     ('9780451524935', '1984', 'Джордж Оруэлл', 'Азбука', 'Антиутопия, критикующая тоталитаризм и экстремальные политические идеологии.', 'https://cdn.azbooka.ru/cv/w1100/08fe16b6-f3dc-4a62-a63a-0ac471bf0e9a.jpg'),
     ('9780316769488', 'Над пропастью во ржи', 'Джером Дэвид Сэлинджер', 'Эксмо', 'Роман о взрослении, рассказывающий о Холдена Колфилде, трудном подростке в Нью-Йорке.', 'https://cdn.eksmo.ru/v2/ITD000000000936146/COVER/cover1__w820.jpg'),
     ('9780140449266', 'Анна Каренина', 'Лев Толстой', 'ACT', 'Сложное исследование любви, брака и общества в России XIX века.', 'https://cdn1.ozone.ru/s3/multimedia-5/6009081221.jpg'),
-    ('9780553213119', 'Отверженные', 'Виктор Гюго', 'Азбука', 'Монументальное произведение о справедливости, любви и искуплении в послереволюционной Франции.', 'https://cdn.azbooka.ru/cv/w1100/8e4f70bd-f412-4f3c-a9cf-b1755601fb97.jpg');
+    ('9780553213119', 'Отверженные', 'Виктор Гюго', 'Азбука', 'Монументальное произведение о справедливости, любви и искуплении в послереволюционной Франции.', 'https://cdn.azbooka.ru/cv/w1100/8e4f70bd-f412-4f3c-a9cf-b1755601fb97.jpg')
 
 CREATE TABLE book_product (
     isbn CHAR(13) PRIMARY KEY REFERENCES book(isbn),
@@ -128,6 +124,77 @@ CREATE TABLE stock (
     PRIMARY KEY (location_id, isbn)
 );
 
+INSERT INTO stock (location_id, isbn, quantity) VALUES 
+	-- Location 00000 - stock
+	('00000', '9781909115026', 3000),
+    ('00000', '9788826070643', 2500),
+    ('00000', '9785040400430', 4000),
+    ('00000', '9780140449136', 3500),
+    ('00000', '9780061120084', 2800),
+    ('00000', '9780192833983', 3100),
+    ('00000', '9780140449181', 2700),
+    ('00000', '9780307277671', 4500),
+    ('00000', '9780743273565', 2900),
+    ('00000', '9780451524935', 5000),
+    ('00000', '9780316769488', 2200),
+    ('00000', '9780140449266', 3800),
+    ('00000', '9780553213119', 3300),
+
+    -- Location 10000
+    ('10000', '9781909115026', 300),
+    ('10000', '9788826070643', 250),
+    ('10000', '9785040400430', 450),
+    ('10000', '9780140449136', 320),
+    ('10000', '9780061120084', 280),
+    
+    -- Location 10001
+    ('10001', '9780192833983', 150),
+    ('10001', '9780140449181', 200),
+    ('10001', '9780307277671', 120),
+    ('10001', '9780743273565', 250),
+    
+    -- Location 10002
+    ('10002', '9780451524935', 180),
+    ('10002', '9780316769488', 300),
+    ('10002', '9780140449266', 130),
+    ('10002', '9780553213119', 170),
+    
+    -- Location 10003
+    ('10003', '9781909115026', 350),
+    ('10003', '9788826070643', 200),
+    ('10003', '9785040400430', 400),
+    
+    -- Location 10004
+    ('10004', '9780140449181', 300),
+    ('10004', '9780307277671', 150),
+    ('10004', '9780451524935', 400),
+    
+    -- Location 20000
+    ('20000', '9780316769488', 200),
+    ('20000', '9780140449266', 130),
+    ('20000', '9780553213119', 220),
+    
+    -- Location 30000
+    ('30000', '9780192833983', 190),
+    ('30000', '9780140449181', 120),
+    ('30000', '9780743273565', 210),
+    
+    -- Location 40000
+    ('40000', '9780451524935', 250),
+    ('40000', '9780316769488', 300),
+    ('40000', '9780553213119', 140),
+    
+    -- Location 50000
+    ('50000', '9780140449136', 150),
+    ('50000', '9780307277671', 220),
+    ('50000', '9780061120084', 180),
+    
+    -- Location 50001
+    ('50001', '9780743273565', 300),
+    ('50001', '9780451524935', 150),
+    ('50001', '9780316769488', 120),
+    ('50001', '9781909115026', 250);
+
 CREATE TABLE orders (
     order_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     customer_id INT NOT NULL REFERENCES person(person_id),
@@ -139,9 +206,11 @@ CREATE TABLE orders (
 );
 
 CREATE TABLE order_items (
-    order_id BIGINT PRIMARY KEY REFERENCES orders(order_id),
-    isbn CHAR(13) NOT NULL REFERENCES stock(isbn),
-    quantity INT NOT NULL CHECK (quantity <= (SELECT quantity FROM stock WHERE stock.location_id = order_items.location_id AND stock.isbn = order_items.isbn)),
+    order_id BIGINT REFERENCES orders(order_id),
+   	location_id CHAR(5) NOT NULL,
+    isbn CHAR(13) NOT NULL,
+    quantity INT NOT NULL,
     status order_status NOT NULL,
-    PRIMARY KEY (order_id, isbn)
+    PRIMARY KEY (order_id, isbn),
+	FOREIGN KEY (location_id, isbn) REFERENCES stock(location_id, isbn)
 );
